a3476111c13cc0dc6ca7a1ad5ed53db53da3af7b
Hamza Wahab <hamzaabdulwahab19@gmail.com>
2025-08-05 11:35:26 +0500

    Initial Commit

---
+++ Scapp/ScappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScappApp.swift
+//  Scapp
+//
+//  Created by Hamza Wahab on 05/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScappTests/ScappTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScappTests.swift
+//  ScappTests
+//
+//  Created by Hamza Wahab on 05/08/2025.
+//
+
+import Testing
+@testable import Scapp
+
+struct ScappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
